b77aff63da23a7618fd6e7ef4707cf7fc0884daf
DuvanSGF <software.bugs.io@gmail.com>
2022-12-19 21:33:22 -0500

    Update PuttyConection.sql

--- admin/PuttyConection.sql
+++ admin/PuttyConection.sql
@@ -7,7 +7,7 @@
 
 1) Setting up the respective network configurations in the virtual machine.
 2) Then activate ethernet protocol ens33 and a3s08
-3) $ifconfig --- Inet de ens33 : [10.0.2.15]
+3) $ifconfig --- Inet de enp0s8 
 
 4)As root user => $root sudo dnf install openssh openssh-server
 5) enp0s8 get inet protocol : 192.168.1.101 
