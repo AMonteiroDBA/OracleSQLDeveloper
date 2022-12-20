8c35902a34f3778eb25c2621af064f339e41343f
DuvanSGF <software.bugs.io@gmail.com>
2022-12-19 21:32:53 -0500

    Create PuttyConection.sql

---
+++ admin/PuttyConection.sql
@@ -0,0 +1,18 @@
+/*
+ Ing. Duván Albeiro Mejia Cortes 
+ Topic: SSH protocol connection to Oracle Linux via PUTTY. 
+ 
+ 
+ 
+
+1) Setting up the respective network configurations in the virtual machine.
+2) Then activate ethernet protocol ens33 and a3s08
+3) $ifconfig --- Inet de ens33 : [10.0.2.15]
+
+4)As root user => $root sudo dnf install openssh openssh-server
+5) enp0s8 get inet protocol : 192.168.1.101 
+6) put the inet into Putty and then connect
+*/
+
+
+
